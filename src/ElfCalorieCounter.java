8b7827d54ce78dbe7c26a4a7872df4726174afcc
Wriceboy41 <waytothelongamez@gmail.com>
2022-12-20 08:52:52 -0700

    day4
    finished day4 part 1

--- src/ElfCalorieCounter.java
+++ src/ElfCalorieCounter.java
@@ -89,16 +89,140 @@ public class ElfCalorieCounter {
 
             throw new RuntimeException(e);
         }
+
         int total = 0;
         while (scr.hasNextLine()){
             String s = scr.nextLine();
+            System.out.println(s);
             char me = s.charAt(2);
             char them = s.charAt(0);
 
+            switch (them){
+                case 'A':
+                    switch (me) {
+                        case 'Y' -> total += 1+3; // rock vs rock
+                        case 'X' -> total += 0+3; //rock vs scissors
+                        case 'Z' -> total += 6+2; // rock vs paper
+                    }
+                 break;
+                case 'B':
+                    switch (me) {
+                        case 'Y' -> total += 3+2; // paper vs paper
+                        case 'X' -> total += 0+1; // paper vs rock
+                        case 'Z' -> total += 6+3; // paper vs scissors
+                    }
+                    break;
+                case 'C':
+                    switch (me) {
+                        case 'Y' -> total += 3+3; //scissors vs scissors
+                        case 'X' -> total += 0+2; // scissors vs paper
+                        case 'Z' -> total += 6+1; // scissors vs rock
+                    }
+            }
 
         }
+        System.out.println(total);
+
+    }
+
+    public static void day3() {
+        String path = "res/day3.txt";
+        File file = new File(path);
+
+        Scanner scr;
+
+        try {
+
+            scr = new Scanner(file);
+
+        } catch (FileNotFoundException e) {
+
+            throw new RuntimeException(e);
+        }
+        int total = 0;
+        while(scr.hasNextLine()){
+            String s = scr.nextLine();
+            String t = scr.nextLine();
+            String u = scr.nextLine();
+            String top = s.substring(0, s.length()/2);
+            String bot = s.substring(s.length()/2);
+
+            String alph = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for (int i = 0; i < s.length(); i++) {
+                for (int j = 0; j < t.length(); j++) {
+                    /* part 1 if (top.charAt(i) == bot.charAt(j)){
+                        System.out.println(top.charAt(i));
+                        total += alph.indexOf(top.charAt(i))+1;
+                        System.out.println(total);
+                        i += 20000000;
+                        break; ***/
+                    for (int k = 0; k < u.length(); k++) {
+                        if(s.charAt(i) == t.charAt(j) && s.charAt(i) == u.charAt(k)){
+                            total += alph.indexOf(s.charAt(i))+1;
+
+                            j += 800;
+                            i += 800;
+                            break;
+                        }
+                    }
+
+                    }
+                }
+
+            }
+
+        System.out.println(total);
+    }
+
+    public static void day4(){
+        String path = "res/day4.txt";
+        File file = new File(path);
+
+        Scanner scr;
+
+        try {
+
+            scr = new Scanner(file);
+
+        } catch (FileNotFoundException e) {
+
+            throw new RuntimeException(e);
+        }
+
+        int total = 0;
+        while(scr.hasNextLine()){
+            String s = scr.nextLine();
+            System.out.println(s);
+            String a = "";
+            String b = "";
+            String y = "";
+            String z = "";
+            int val = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) != '-' && s.charAt(i) != ','){
+                    if(val == 0){
+                    a = a + s.charAt(i);
+                    } else if(val == 1){
+                        b = b + s.charAt(i);
+                    }else if(val == 2){
+                        y = y + s.charAt(i);
+                    }else if(val == 3){
+                        z = z + s.charAt(i);
+                    }
+                } else{
+                    val++;
+                }
+            }
 
 
+            if(Integer.parseInt(y) <= Integer.parseInt(b) && Integer.parseInt(a) <= Integer.parseInt(y)){
+                total++;
+                System.out.println(total);
+
+            }
+
+        }
+        System.out.println(total);
     }
 
     public static void day5() {
@@ -164,6 +288,6 @@ public class ElfCalorieCounter {
 
 
     public static void main(String[] args) {
-
+        day4();
     }
 }
